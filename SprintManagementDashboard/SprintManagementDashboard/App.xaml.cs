3b04358c296582f8322626552d8066fd70c5cc82
Hariram4428 <hariram.subramanian@syncfusion.com>
2025-12-29 20:39:03 +0530

    Code changes updated

---
+++ SprintManagementDashboard/SprintManagementDashboard/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace SprintManagementDashboard
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var isDesktop = DeviceInfo.Idiom == DeviceIdiom.Desktop || DeviceInfo.Idiom == DeviceIdiom.TV;
+            Page root = isDesktop
+                ? new DesktopPage()
+                : new MobilePage();
+
+            return new Window(root);
+        }
+    }
+}
